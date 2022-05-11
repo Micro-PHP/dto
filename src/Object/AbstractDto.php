ce345fda8e7b5bf9400f0b5e96ac557ceeb15b51
Stanislau Komar <stanislau_komar@epam.com>
2022-05-11 18:36:41 +0400

    Implements array access and iterable

--- src/Object/AbstractDto.php
+++ src/Object/AbstractDto.php
@@ -2,7 +2,9 @@
 
 namespace Micro\Library\DTO\Object;
 
-abstract class AbstractDto
+use Traversable;
+
+abstract class AbstractDto implements \ArrayAccess, \IteratorAggregate, \JsonSerializable
 {
     /**
      * @param array|null $sourceData
@@ -15,7 +17,24 @@ abstract class AbstractDto
     /**
      * @return array
      */
+    protected abstract function attributesMetadata(): array;
+
+    /**
+     * @return array
+     */
     public abstract function toArray(): array;
+
+    /**
+     * @param string $attribute
+     *
+     * @return array|null
+     */
+    protected function getAttributeMetadata(string $attribute): ?array
+    {
+       $meta = $this->attributesMetadata();
+
+       return $meta[$attribute] ?? null;
+    }
 
     /**
      * @param array|null $sourceData
@@ -45,7 +64,74 @@ abstract class AbstractDto
     }
 
     /**
-     * @return array
+     * {@inheritDoc}
      */
-    protected abstract function attributesMetadata(): array;
+    public function offsetExists(mixed $offset): bool
+    {
+        return !!$this->getAttributeMetadata($offset);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function offsetGet(mixed $offset): mixed
+    {
+        return $this->executeAction('get', $offset, null);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        $this->executeAction('set', $offset, $value);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function offsetUnset(mixed $offset): void
+    {
+        $this->offsetSet($offset, null);
+    }
+
+    /**
+     * @return Traversable
+     */
+    public function getIterator(): Traversable
+    {
+        return (function () {
+            foreach ($this->attributesMetadata() as $attributeName => $meta) {
+                yield $attributeName => $this->offsetGet($attributeName);
+            }
+        })();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function jsonSerialize(): mixed
+    {
+        return json_encode($this->toArray());
+    }
+
+    /**
+     * @param string $action
+     * @param string $property
+     * @param mixed $value
+     * @return mixed
+     */
+    protected function executeAction(string $action, string $property, mixed $value): mixed
+    {
+        $meta = $this->getAttributeMetadata($property);
+        if(!$meta) {
+            throw new \InvalidArgumentException(
+                sprintf('Property "%s" is not declared in the class "%s".', $property, get_class($this))
+            );
+        }
+
+        $actionName = $meta['actionName'];
+
+        return $this->{$action . $actionName}($value);
+    }
 }
