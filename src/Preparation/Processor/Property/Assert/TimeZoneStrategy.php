f3701b7d0a9f4d2a13a5370aeac20a0d9ceb8e42
Stanislau Komar <Stanislau_Komar@epam.com>
2023-02-18 02:59:40 +0400

    v1.6.2 [improvements] XSD: readable timezones configuration

--- src/Preparation/Processor/Property/Assert/TimeZoneStrategy.php
+++ src/Preparation/Processor/Property/Assert/TimeZoneStrategy.php
@@ -23,9 +23,43 @@ class TimeZoneStrategy extends AbstractConstraintStrategy
 
         $parent['countryCode'] = $config['country_code'] ?? null;
         $parent['intlCompatible'] = $this->stringToBool($config['intl_compatible'] ?? 'false');
-        $parent['zone'] = (int) ($config['zone'] ?? \DateTimeZone::ALL);
+
+        $this->applyTz($parent, $config);
 
         return array_filter($parent);
+    }
+
+    /**
+     * @param array<string, mixed> $args
+     * @param array<string, mixed> $config
+     *
+     * @return void
+     */
+    protected function applyTz(array &$args, array $config): void
+    {
+        $zone = 0;
+        if (empty($config['zone'])) {
+            $args['zone'] = \DateTimeZone::ALL;
+
+            return;
+        }
+
+        $zones = $config['zone'];
+        foreach ($zones as $z) {
+            $zone |= $this->getTzValue($z['value']);
+        }
+
+        $args['zone'] = $zone;
+    }
+
+    protected function getTzValue(string $zoneName): int
+    {
+        $constName = sprintf('%s::%s', \DateTimeZone::class, $zoneName);
+        if (!\defined($constName)) {
+            throw new \InvalidArgumentException(sprintf('Time zone `%s` is not defined in class %s', $zoneName, \DateTimeZone::class));
+        }
+
+        return \constant($constName);
     }
 
     protected function getValidatorProperty(): string
